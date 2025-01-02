5a6231409338631324720aa2084c62fd8784a97a
SumanBanikSB <baniksuman9434@gmail.com>
2025-01-02 12:12:33 +0530

    indent fixes

--- main.py
+++ main.py
@@ -7,29 +7,23 @@ class FairyTaleProcessor:
 		self.stack = []
 
 	def process_ntvh(self, line):
-
 		self.stack.append(line)
 
 	def process_ntvhi(self, line):
-
 		self.stack.append(line)
 
 	def process_file(self):
 		try:
-
 			with open(self.input_file, 'r') as input_f, \
          open(self.code_file, 'r') as code_f, \
          open(self.output_file, 'w') as output_f:
-
 				command = code_f.readline().strip()
-
 				for line in input_f:
 					line = line.strip()
 					if command == "NTVH":
 						self.process_ntvh(line)
 					elif command == "NTVHI":
 						self.process_ntvhi(line)
-
 				while self.stack:
 					output_f.write(self.stack.pop() + '\n')
 
